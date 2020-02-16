aa4390d45891f715bd16d4e8ef3e1416a8210bfd
VICSOLWANG <413834325@qq.com>
2020-02-16 10:12:48 +0800

    feat: add test

---
+++ test.js
@@ -0,0 +1,22 @@
+/**
+ * Created by VicSolWang.
+ * Date: 2020-02-16 0:42
+ * Email: vic.sol.wang@gmail.com
+ */
+
+const test = require('ava');
+const { CLIEngine } = require('eslint');
+const rule = require('./index');
+
+const isObject = (obj) => typeof obj === 'object' && obj !== null;
+
+test('Test basic properties of config.', (t) => {
+	t.true(isObject(rule.env));
+	t.true(isObject(rule.rules));
+});
+
+test('Load config in eslint to validate all rule syntax is correct.', (t) => {
+	const cli = new CLIEngine();
+	const code = 'const foo = 1;\nconst bar = () => {};\nbar(foo);\n';
+	t.is(cli.executeOnText(code).errorCount, 0);
+});
