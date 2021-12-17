00ae10e847a57282dd40bb2b1fc2b329cb6dedc3
VICSOLWANG <413834325@qq.com>
2021-12-17 15:29:44 +0800

    feat: eslint update to v8 and replace references and functions of test.js

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 /**
- * Created by VicSolWang.
- * Date: 2020-02-16 0:42
+ * Created by VICSOLWANG.
+ * Date: 2020/02/16 0:42
  * Email: vic.sol.wang@gmail.com
  */
 
 const test = require('ava');
-const { CLIEngine } = require('eslint');
+const { ESLint } = require('eslint');
 const rule = require('./index');
 
 const isObject = (obj) => typeof obj === 'object' && obj !== null;
@@ -13,13 +13,13 @@ const isObject = (obj) => typeof obj === 'object' && obj !== null;
 const isArray = (array) => Array.isArray(array);
 
 test('Test basic properties of config.', (t) => {
-	t.true(isObject(rule.env));
-	t.true(isArray(rule.plugins));
-	t.true(isObject(rule.rules));
+  t.true(isArray(rule.plugins));
+  t.true(isObject(rule.rules));
 });
 
-test('Load config in eslint to validate all rule syntax is correct.', (t) => {
-	const cli = new CLIEngine();
-	const code = 'const foo = 1;\nconst bar = () => {};\nbar(foo);\n';
-	t.is(cli.executeOnText(code).errorCount, 0);
+test('Load config in eslint to validate all rule syntax is correct.', async (t) => {
+  const eslint = new ESLint();
+  const code = 'const foo = 1;\nconst bar = () => {};\nbar(foo);\n';
+  const results = await eslint.lintText(code);
+  t.is(((results || [])[0] || {}).errorCount, 0);
 });
