941906e2fa87899290f1ec35b56889fdc8f398ce
VICSOLWANG <413834325@qq.com>
2021-12-23 15:47:35 +0800

    fix: improve test

---
+++ ava.config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by VICSOLWANG.
+ * Date: 2021/12/22 22:19
+ * Email: vic.sol.wang@gmail.com
+ */
+
+export default {
+  files: ['test/test.js'],
+};
---
+++ test/example/type.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by VICSOLWANG.
+ * Date: 2021/12/22 23:38
+ * Email: vic.sol.wang@gmail.com
+ */
+
+const a = (): void => {};
+a();
