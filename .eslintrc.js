bc0a394d018fd81ea9524b1350f4e6749025f18a
VICSOLWANG <413834325@qq.com>
2023-11-13 11:07:07 +0800

    chore: change the eslint and eslintignore config file type becauseof the flat config

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by VICSOLWANG.
- * Date: 2020/02/01 0:35
- * Email: vic.sol.wang@gmail.com
- */
-
-const config = require('./src/config');
-
-module.exports = config;
---
+++ eslintignore.config.js
@@ -0,0 +1,17 @@
+/**
+ * Created by VICSOLWANG.
+ * Date: 2023/11/10 14:14
+ * Email: vic.sol.wang@gmail.com
+ */
+
+export default [
+  '.husky/*',
+  'coverage/*',
+  'coverage.*',
+  '.github/*',
+  'node_modules/*',
+  '.idea/*',
+  '.vscode/*',
+  'target/*',
+  '.DS_Store/*',
+];
